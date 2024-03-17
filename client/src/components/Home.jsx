a6c65612844df2a7858cbea3068c0e5256fcbf56
P-Bhuvanesh <pbhuvanesh345@gmail.com>
2024-03-17 16:55:03 +0530

    Home Component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import Navbar from "./Navbar";
-// import "./styles/Home.css";
+import "./styles/Home.css";
 const Home = () => {
   return (
     <div className="wrapper">
