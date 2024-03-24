db631e6b4a9a84c27a04d1f71a440b38e1d5b3ce
P-Bhuvanesh <pbhuvanesh345@gmail.com>
2024-03-24 17:04:27 +0530

    Derm Page

--- client/src/components/Derm.jsx
+++ client/src/components/Derm.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import "../styles/Derm.css";
 const Derm = () => {
   return (
-    <div>Derm</div>
-  )
-}
+    <div className="derm-parent">
+      <div className="derm-left">
+        Recommending the Nearby Dermatologist
+        <img src="src/assets/Map.png" alt="Maps" sizes="" srcset="" />
+      </div>
+      <div className="derm-right">
+        <ul>
+          <li>Clinic Name:</li>          
+          <li className='mapdb'>____________</li>          
+          <li>Contact Details: </li>          
+          <li className='mapdb'>____________</li>          
+          <li>Distance From Here:</li>          
+          <li className='mapdb'>____ KM</li>          
+          <li>Timings:</li>          
+          <li className='mapdb'>____________</li>          
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Derm
+export default Derm;
